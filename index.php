a300a42e9722c2306c95ce5859cabd8ca312e725
rudenskaya <rudenskaya96@yandex.ru>
2019-01-28 19:25:05 +0300

    Добавлены файлы

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Проект #3</title>
+    <link rel="stylesheet" href="/style/style.css">
 </head>
 <body>
     
+    <h1>Код проекта #3</h1>
 
 
-
-    
+    <script src="/JS/main.js"></script>    
 </body>
 </html>
